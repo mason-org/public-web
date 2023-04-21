48ddc35b8a9c95b081a8a02365cd989948561443
William Boman <william@redwill.se>
2023-04-21 21:42:51 +0200

    vercel init gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Mason - a package manager for Neovim`,
+    siteUrl: `https://mason-registry.dev/`,
+  },
+  plugins: [],
+};
